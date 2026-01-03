c44ecab6969daadb6fcca5cd820c35f42685f584
its-abhi-IT <abhig636@gmail.com>
2026-01-04 01:55:10 +0530

    added folder

---
+++ DSA/DSA/Array/SecondLargestFinder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DSA.Array;
+
+public class SecondLargestFinder
+{
+    int[] arr = [1, 2, 4, 5, 10, 21, 3, 50];
+
+    public SecondLargestFinder()
+    {
+        Console.WriteLine("");
+    }
+    public void large(int[] arr)
+    {
+        for (int v =0; arr;)
+        {
+
+        }
+    }
+}
---
+++ DSA/DSA/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+using DSA.Array;
+
+Console.WriteLine("Hello, DSA!");
+Console.WriteLine("Please select any of one");
+string programList = @"
+1.Array
+2.Soon
+0.Exit";
+Console.WriteLine(programList);
+
+string userInput = Console.ReadLine();
+
+if (string.IsNullOrEmpty(userInput))
+{
+    Console.WriteLine("You canceled the program");
+    Console.WriteLine("Have a nice day!");
+    Environment.Exit(0);
+}
+int input = Convert.ToInt32(Console.ReadLine());
+
+if (input ==0)
+{
+    Console.WriteLine("Oops! not selected");
+    
+    Environment.Exit(0);
+}
+
+switch (input)
+{
+    case 1:
+        SecondLargestFinder secondLargest = new SecondLargestFinder();
+        break;
+}
